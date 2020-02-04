1d5585c0d9882838b4f075abb1dfebc0e336337c
Nadim Bahadoor <nbahadoor@gmail.com>
2020-02-04 16:39:05 +0400

    Added some more basic String Scala exercises

---
+++ source-code/scala-exercises/src/main/scala/com/allaboutscala/exercises/basics/StringAndBasics_06.scala
@@ -0,0 +1,36 @@
+package com.allaboutscala.exercises.basics
+
+/**
+  * Created by Nadim Bahadoor on 01/20/2020.
+  *
+  * Tutorial: Source code for the scala-exercises section at http://allaboutscala.com
+  *
+  * [[http://allaboutscala.com/tutorials/scala-exercises/]]
+  *
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object StringAndBasics_06 extends App {
+
+  // 6. Create a Scala program that will ask the following question to a customer: "What is your favorite movie of all times?".
+  // Output:
+  /**
+    * [customersFavoriteMovie] is totally awesome!
+    */
+
+    import scala.io.StdIn._
+    val favoriteMovie = readLine("What is your favorite movie of all times?")
+    println(s"$favoriteMovie is totally awesome!")
+}
